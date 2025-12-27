9c4c49086e8f824c351f7ff9fe5e5e98d26c7129
Gromiusz <kgorka22@gmail.com>
2025-12-27 11:48:19 +0100

    build config

---
+++ include/public/osqp_configure.h
@@ -0,0 +1,40 @@
+#ifndef OSQP_CONFIGURE_H
+#define OSQP_CONFIGURE_H
+
+/* Minimal manually-generated config for PX4 build */
+
+/* OSQP_ENABLE_DEBUG */
+/* #undef OSQP_ENABLE_DEBUG */
+
+/* Operating system */
+#define IS_LINUX
+
+/* Algebra backend */
+#define OSQP_ALGEBRA_BUILTIN
+/* #undef OSQP_ALGEBRA_MKL */
+/* #undef OSQP_ALGEBRA_CUDA */
+
+/* Codegen / profiler options */
+/* #undef OSQP_CODEGEN */
+/* #undef OSQP_PROFILER_ANNOTATIONS */
+/* #undef OSQP_ENABLE_DERIVATIVES */
+
+/* Embedded mode */
+#define OSQP_EMBEDDED_MODE (0)
+
+/* Custom allocators / printing */
+/* #undef OSQP_CUSTOM_MEMORY */
+/* #undef OSQP_ENABLE_PRINTING */
+/* #undef OSQP_CUSTOM_PRINTING */
+
+/* Profiling / interrupt */
+/* #undef OSQP_ENABLE_PROFILING */
+/* #undef OSQP_ENABLE_INTERRUPT */
+
+/* Precision / integer size */
+/* #undef OSQP_USE_FLOAT */
+/* #undef OSQP_USE_LONG */
+
+/* #undef OSQP_PACK_SETTINGS */
+
+#endif /* OSQP_CONFIGURE_H */
